25bf69c4deb5a2b4f6af9c053667908773391daa
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-07-19 23:36:07 +0530

    upload and view filename functionality added

--- main.js
+++ main.js
@@ -1,30 +1,6 @@
-function snap(){
-    var win
-    for (let i = 0; i < 5; i++) {
-        win = window.open('', '_blank');    
-    }
-    
-    win.close();
-}
-
-function testSnap(windowInfoArray) {
-    var some = window.name();
-    console.log(some);
-}
-  
-chrome.windows.getAll({populate:true}, getAllOpenWindows);
-
-function getAllOpenWindows(winData) {
-
-  var tabs = [];
-  for (var i in winData) {
-    if (winData[i].focused === true) {
-        var winTabs = winData[i].tabs;
-        var totTabs = winTabs.length;
-        for (var j=0; j<totTabs;j++) {
-          tabs.push(winTabs[j].url);
-        }
-    }
-  }
-  console.log(tabs);
+function upload() {
+  var path = document.getElementById("upload");
+  var filename = path.files[0].name;
+  document.getElementById("result").innerHTML = "File name: " + filename;
+  console.log(filename);
 }
