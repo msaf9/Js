cfb862f108ee6253951dd8aa1e2eca6b9db23ed1
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-07-19 22:45:21 +0530

    WIP: upload feature implementation

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ function snap(){
     win.close();
 }
 
+function testSnap(windowInfoArray) {
+    var some = window.name();
+    console.log(some);
+}
+  
+chrome.windows.getAll({populate:true}, getAllOpenWindows);
+
+function getAllOpenWindows(winData) {
+
+  var tabs = [];
+  for (var i in winData) {
+    if (winData[i].focused === true) {
+        var winTabs = winData[i].tabs;
+        var totTabs = winTabs.length;
+        for (var j=0; j<totTabs;j++) {
+          tabs.push(winTabs[j].url);
+        }
+    }
+  }
+  console.log(tabs);
+}
