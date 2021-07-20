a4837c3d8572a1e01217814f858b28289d5dd145
Sahil Afrid Farookhi <msafarookhi@gmail.com>
2021-07-20 23:52:00 +0530

    type added

--- main.js
+++ main.js
@@ -3,20 +3,22 @@ function upload() {
   var size = document.getElementById("upload").files.length;
   console.log(size);
   const filename = [];
+  const filetype = [];
+  const getExtension = (str) => str.slice(str.lastIndexOf("."));
   var i = 0;
   for (i = 0; i < size; i++) {
     filename[i] = path.files[i].name;
+    filetype[i] = getExtension(filename[i]);
 
     document.getElementById("result").innerHTML = "File name: " + filename[i];
+    document.getElementById("type").innerHTML = "File type: " + filetype[i];
     console.log(filename[i]);
+    console.log(filetype[i]);
   }
   document.getElementById("result").innerHTML = JSON.stringify(
     "File name: " + filename
   );
-  // var filename = path.files[0].name;
-  // var filename1 = path.files[1].name;
-  // document.getElementById("result").innerHTML = "File name: " + filename;
-  // document.getElementById("result").innerHTML = "File name: " + filename1;
-  // console.log(filename);
-  // console.log(filename1);
+  document.getElementById("type").innerHTML = JSON.stringify(
+    "File type: " + filetype
+  );
 }
